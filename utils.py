072e2a8cc32494017045cea3997070ca2750fbb9
Jacob Nathaniel Wessell <jwessell6@atl1-1-03-013-8-0.pace.gatech.edu>
2024-11-07 21:07:58 -0500

    modified code to work with OpenAI's gym environments

--- utils.py
+++ utils.py
@@ -13,11 +13,26 @@ def make_prompt(text, topic):
     
 #generate a conversation for our in-context few-shot learning. This process can be made more complex in the future,
 #but for now a conversation will be built from a base prompt and a collection of description-answer pairs.
-def make_conversation(descriptions, answers):
+def make_conversation(n_shots, answers):
     #configure a base prompt here. We can handle few-shot learning with this function if needed.
-    base_prompt = ""
-    def make_prompt(text):
-        prompt = base_prompt.format(desc = text)
+    def begin_conversation():
+        system = {
+        "role": "system",
+        "content": [
+            {"type": "text", "text": """A chat between a human and a helpful AI assistant. 
+                                    The human needs help landing the the space probe safely and the assistant provides the best action at each step.
+                                    The goal is to land the probe safely on the surface of the moon and, preferably, between the flags.
+                                    At each step, the assistant can instruct the human to fire the main thruster, fire the left thruster, 4
+                                    fire the right thruster, or do nothing. The human cannot fire multiple thrusters at once."""},
+            ],
+        }
+        return system 
+    base_prompt = "The image depicts the current state of my lunar lander. What action should I take to most improve my chances of landing safely between the flags? Responsd with either \'fire left\', \'fire right\', \'fire main\', \'or do nothing.\'"
+    def make_prompt(text = None):
+        if text is not None:
+            prompt = base_prompt.format(desc = text)
+        else:
+            prompt = base_prompt
         user =   {
             "role": "user",
             "content": [
@@ -26,6 +41,7 @@ def make_conversation(descriptions, answers):
                 ],
         }
         return user
+    #make_answer only needed in a few-shot setting
     def make_answer(ans):
         answer = {
             "role": "assistant",
@@ -35,8 +51,10 @@ def make_conversation(descriptions, answers):
             }
         return answer
     conversation = []
-    for i in range(len(descriptions)-1):
-        conversation.append(make_prompt(descriptions[i]))
+    conversation.append(begin_conversation())
+    #we can add additional information to our prompt and few shot examples.
+    for i in range(n_shots):
+        conversation.append(make_prompt())
         conversation.append(make_answer(answers[i]))
-    conversation.append(make_prompt(descriptions[-1]))
+    conversation.append(make_prompt())
     return conversation
--- vlm.py
+++ vlm.py
@@ -1,52 +1,51 @@
 import pandas as pd
 import numpy as np
 import torch 
-from transformers import AutoProcessor, LlavaNextForConditionalGeneration, BitsAndBytesConfig
+from transformers import AutoProcessor, LlavaOnevisionForConditionalGeneration, BitsAndBytesConfig
 import pickle
 import sys
 import argparse
+import gym
+from utils import make_conversation
+from PIL import Image
 
-
-def run_hf(topic, batch_size = 1, max_gen = 5):
+def run_hf(environment_name):
     #quantization: not necessary but can improve inference speed at the cost of some performance
     '''for i in range(torch.cuda.device_count()):
            print(torch.cuda.get_device_properties(i).name)
-    torch.cuda.set_device(1)'''
+    torch.cuda.set_device(1)
     quantization_config = BitsAndBytesConfig(
         load_in_8bit=True
-    )
-    model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", torch_dtype=torch.float16, device_map = 'auto')
-    processor = AutoProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
-    processor.tokenizer.padding_side = 'left'
-    processor.tokenizer.max_length = 1700
-    processor.tokenizer.truncation = True
-    model.padding_side = 'right'
+    )'''
+    model = LlavaOnevisionForConditionalGeneration.from_pretrained("llava-hf/llava-onevision-qwen2-7b-ov-hf", torch_dtype=torch.float16, device_map="auto")
     model.eval()
+    #print(model.device)
+    processor = AutoProcessor.from_pretrained("llava-hf/llava-onevision-qwen2-7b-ov-hf")
     res = []
-    for i in range(0, len(data)-batch_size, batch_size):
-        print(i)
-        eg, sources, keys = data.getbatch(i, min(len(data) -1,i+batch_size))
-        eg = eg.to(model.device)
-        #print(eg['input_ids'][0].shape)
-        with torch.no_grad():
-            #gen = model.generate(**eg, max_new_tokens=max_gen)
-            try:
-                gen = model.generate(**eg, max_new_tokens=max_gen)
-            except RuntimeError:
-                print("Index: " + str(i))
-                for i in range(len(eg['input_ids'])):
-                    print(eg['input_ids'][i].shape)
-                for i in range(len(eg['image_sizes'])):
-                    print(eg['image_sizes'][i])
-        for i in range(len(gen)):
-            res.append([sources[i], keys[i], processor.batch_decode(gen[i][-max_gen:],skip_special_tokens=True, clean_up_tokenization_spaces=False)])
-    name = "zeroshot" + topic + ".pkl"
-    with open(name, 'wb') as file:
-        pickle.dump(res, file)
+    demo_environment = gym.make(environment_name, render_mode='rgb_array')
+    state, info = demo_environment.reset()
+    for i in range(35):
+        action = np.random.randint(low = 0, high = 4)
+        state, _, _, _, _ = demo_environment.step(action)
+    image = demo_environment.render()
+    im = Image.fromarray(image)
+    im.save("../agent_outputs/your_file.jpeg")
+    eg = Image.open("../agent_outputs/eg.jpeg") 
+    eg = np.array(eg)
+    images = [eg, image]
+    chat = make_conversation(1, ["Fire right."])
+    prompt = processor.apply_chat_template(chat, add_generation_prompt=True)
+    inputs = processor(images=images, text=prompt, return_tensors="pt").to("cuda:0", torch.float16)
+    output = model.generate(**inputs, max_new_tokens=5)
+    instruction = processor.decode(output[0], skip_special_tokens=True)
+    print(instruction[len(inputs):])
+    with open("../agent_outputs/Output.txt", "w") as text_file:
+        text_file.write(instruction[len(inputs):])
+    return
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--topic',help='Which variable to code for')
-    parser.add_argument('-b', type = int, default = 32, help='Batch Size')
+    parser.add_argument('--env',type=str, default = 'LunarLander-v2', help='Which environment to use')
     args = parser.parse_args()
-    run_hf(args.topic, args.b)
+    run_hf(args.env)
