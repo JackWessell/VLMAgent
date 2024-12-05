796de1a529af34c522811905b35f40e3b474d339
Jacob Nathaniel Wessell <jwessell6@atl1-1-03-004-5-2.pace.gatech.edu>
2024-12-05 09:42:09 -0500

    Changed some functionalities and added the snake environment.

--- utils.py
+++ utils.py
@@ -1,33 +1,63 @@
 #can add different prompts for different tasks. For our project, we can describe different environments and their respective goals.
-topics = []
-def make_prompt(text, topic):
-    prompt = topics[topic].format(desc = text)
-    user =   {
-        "role": "user",
-        "content": [
-            {"type": "image"},
-            {"type": "text", "text": prompt },
-            ],
+system_prompts = {
+    "Test" : "A chat between a human and a helpful AI assistant.",
+    "LunarLander-v2" : """A chat between a human and a helpful AI assistant. 
+                            The human needs help landing the the space probe safely and the assistant provides the best action at each step.
+                            The goal is to land the probe safely on the surface of the moon and, preferably, between the flags.
+                            At each step, the assistant can instruct the human to fire the main thruster, fire the left thruster, 4
+                            fire the right thruster, or do nothing. The human cannot fire multiple thrusters at once.""",
+    "snake-v0" : """ A chat between a human and a helpful AI assistant. The human is trying to play the popular game snake and needs help understanding
+                    what move to make at each point."""
     }
-    return user
-    
+base_prompts = {
+    "Test" : "Can you describe to me what you see in the image? What do you think is happening?",
+     "LunarLander-v2" : """The image depicts the current position of my lunar lander. What action should I take to most improve my chances of landing safely between the flags? Respond with                            either \'fire left\', \'fire right\', \'fire main\', \'or do nothing.\'""",
+    "snake-v0" : """The image depicts the current state of my game of snake. In what direction should the snake, with its red head and black body, move to eat the blue cube? Please explain your answer given what you know about the game of snake. Include details such as where the snake is relative to the cube in your answer."""
+    }
+def get_example(environment):
+    if environment == "LunarLander-v2":
+        return "Fire Right."
+    if environment == "snake-v0":
+        return "left"
+
+def decode(environment, instruction):
+    if environment == "LunarLander-v2":
+        if "do nothing" in instruction:
+            return 0
+        if "fire left" in instruction:
+            return 1
+        if "fire main" in instruction:
+            return 2
+        if "fire right" in instruction:
+            return 3
+        else:
+            return -1
+    if environment == "snake-v0":
+        if "up" in instruction:
+            return 0
+        if "right" in instruction:
+            return 1
+        if "down" in instruction:
+            return 2
+        if "left" in instruction:
+            return 3
+        else:
+            return -1
 #generate a conversation for our in-context few-shot learning. This process can be made more complex in the future,
 #but for now a conversation will be built from a base prompt and a collection of description-answer pairs.
-def make_conversation(n_shots, answers):
+def make_conversation(n_shots, environment, answers):
     #configure a base prompt here. We can handle few-shot learning with this function if needed.
+    base_prompt = base_prompts[environment]
+    system_prompt = system_prompts[environment]
     def begin_conversation():
         system = {
         "role": "system",
         "content": [
-            {"type": "text", "text": """A chat between a human and a helpful AI assistant. 
-                                    The human needs help landing the the space probe safely and the assistant provides the best action at each step.
-                                    The goal is to land the probe safely on the surface of the moon and, preferably, between the flags.
-                                    At each step, the assistant can instruct the human to fire the main thruster, fire the left thruster, 4
-                                    fire the right thruster, or do nothing. The human cannot fire multiple thrusters at once."""},
+            {"type": "text", "text": system_prompt},
             ],
         }
         return system 
-    base_prompt = "The image depicts the current state of my lunar lander. What action should I take to most improve my chances of landing safely between the flags? Responsd with either \'fire left\', \'fire right\', \'fire main\', \'or do nothing.\'"
+
     def make_prompt(text = None):
         if text is not None:
             prompt = base_prompt.format(desc = text)
--- vlm.py
+++ vlm.py
@@ -1,51 +1,83 @@
+#NOTE: The code below is configured to make running the code possible in Pace-ICE ondemand. You should either change the lines below to reflect your virtual env or comment them out.
+import sys
+sys.executable = 'miniconda3/envs/DRL/bin/python3.10'
+sys.path += ['/home/hice1/jwessell6/DRL/VLMAgent/Gym-Snake', '/home/hice1/jwessell6/miniconda3/envs/DRL/lib/python3.10', 
+    '/home/hice1/jwessell6/miniconda3/envs/DRL/lib/python3.10/site-packages', '/home/hice1/jwessell6/miniconda3/envs/DRL/lib/python3.10/lib-dynload', '/home/hice1/jwessell6/.local/bin']
+
 import pandas as pd
 import numpy as np
 import torch 
 from transformers import AutoProcessor, LlavaOnevisionForConditionalGeneration, BitsAndBytesConfig
 import pickle
-import sys
 import argparse
 import gym
-from utils import make_conversation
+import utils 
 from PIL import Image
+
+import gym_snake
 
 def run_hf(environment_name):
     #quantization: not necessary but can improve inference speed at the cost of some performance
     '''for i in range(torch.cuda.device_count()):
            print(torch.cuda.get_device_properties(i).name)
-    torch.cuda.set_device(1)
+    torch.cuda.set_device(1)'''
     quantization_config = BitsAndBytesConfig(
         load_in_8bit=True
-    )'''
+    )
     model = LlavaOnevisionForConditionalGeneration.from_pretrained("llava-hf/llava-onevision-qwen2-7b-ov-hf", torch_dtype=torch.float16, device_map="auto")
     model.eval()
     #print(model.device)
     processor = AutoProcessor.from_pretrained("llava-hf/llava-onevision-qwen2-7b-ov-hf")
-    res = []
-    demo_environment = gym.make(environment_name, render_mode='rgb_array')
-    state, info = demo_environment.reset()
-    for i in range(35):
+    #setup our one-shot example
+    eg_img = Image.open(f"FewShotEgs/{environment_name}.jpg") 
+    eg_img = np.array(eg_img)
+    eg_txt = [utils.get_example(environment_name)]
+    if environment_name == "snake-v0":
+        env = gym.make('snake-v0')
+        state = env.reset()
+    else:
+        env = gym.make(environment_name, render_mode='rgb_array')
+        state, info = env.reset()
+    done = False
+    '''for i in range(35):
         action = np.random.randint(low = 0, high = 4)
-        state, _, _, _, _ = demo_environment.step(action)
-    image = demo_environment.render()
-    im = Image.fromarray(image)
-    im.save("../agent_outputs/your_file.jpeg")
-    eg = Image.open("../agent_outputs/eg.jpeg") 
-    eg = np.array(eg)
-    images = [eg, image]
-    chat = make_conversation(1, ["Fire right."])
-    prompt = processor.apply_chat_template(chat, add_generation_prompt=True)
-    inputs = processor(images=images, text=prompt, return_tensors="pt").to("cuda:0", torch.float16)
-    output = model.generate(**inputs, max_new_tokens=5)
-    instruction = processor.decode(output[0], skip_special_tokens=True)
-    print(instruction[len(inputs):])
-    with open("../agent_outputs/Output.txt", "w") as text_file:
-        text_file.write(instruction[len(inputs):])
+        state, _, _, _, _ = env.step(action)'''
+    i = 0
+    while not done:
+        if environment_name == 'snake-v0':
+            image = state
+        else:
+            image = env.render()
+        im = Image.fromarray(image)
+        im.save(f"agent_outputs/{environment_name}_{i}.jpg")
+        
+        images = [image]#[eg_img, image]
+        chat = utils.make_conversation(0, environment_name,"") #eg_txt)
+        prompt = processor.apply_chat_template(chat, add_generation_prompt=True)
+        inputs = processor(images=images, text=prompt, return_tensors="pt").to("cuda:0", torch.float16)
+        output = model.generate(**inputs, max_new_tokens=5)
+        instruction = processor.decode(output[0], skip_special_tokens=True)
+        action = utils.decode(environment_name, instruction[len(inputs) : ].lower())
+        print(action)
+        print(instruction)
+        if action == -1:
+            print("bollocks")
+            action = np.random.randint(low = 0, high = 4)
+        state, reward, done, trunc = env.step(action)
+        print(state)
+        print(reward)
+        print(done)
+        print(trunc)
+        i += 1
+    
     return
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--env',type=str, default = 'LunarLander-v2', help='Which environment to use')
+    parser.add_argument('--env',type=str, default = 'snake-v0', help='Which environment to use. Currently supports snake and lunar lander.')
     args = parser.parse_args()
-    run_hf(args.env)
+    if args.env not in ["LunarLander-v2", "snake-v0"]:
+        print("Environment not supported!!!")
+    else:
+        run_hf(args.env)
